d2258d063dcffe8c4f172b06af163f458fa3c18a
9mans <rbska1234@naver.com>
2025-02-16 08:38:19 +0900

    vue project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import FrontWheel from "@/components/FrontWheel.vue";
+import RearWheel from "@/components/RearWheel.vue";
+</script>
+
+<template>
+  <Front-wheel/>
+  <RearWheel/>
+</template>
